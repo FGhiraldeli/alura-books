62c226705bb4791b705294f35f1f576be17ed0c2
Flávio Ghiraldeli Botelho De Paula <ghiraldeli26@gmail.com>
2026-01-05 17:32:51 -0300

    Using styled-components

--- src/componentes/IconesHeader/index.js
+++ src/componentes/IconesHeader/index.js
@@ -1,18 +1,26 @@
 import perfil from '../../imagens/perfil.svg'
 import sacola from '../../imagens/sacola.svg'
-import './estilo.css'
+import styled from 'styled-components'
 
+const Icone = styled.li`
+    margin-right: 40px;
+    width: 25px;
+`
 
+const Icones = styled.ul`
+    display: flex;
+    align-items: center;
+`
 
 const icones = [perfil, sacola]
 
 function IconesHeader() {
     return (
-        <ul className='icones'>
-            {icones.map((icone) => (
-                <li className='icone' key={icone}><img src={icone} alt='' ></img></li>
+        <Icones>
+            {icones.map((icone, index) => (
+                <Icone key={index}><img src={icone} alt='' ></img></Icone>
             ))}
-        </ul>
+        </Icones>
     )
 }
 
