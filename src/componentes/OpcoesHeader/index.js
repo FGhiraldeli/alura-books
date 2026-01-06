3c6d478ebc12f4bbc14b6d3476670898f97180d0
Flávio Ghiraldeli Botelho De Paula <ghiraldeli26@gmail.com>
2026-01-06 09:37:37 -0300

    Using styled-component instead of using a css file for styles

--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,20 +1,33 @@
-import './estilo.css'
+import styled from 'styled-components'
 
 
 const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
 
+const Opcao = styled.li`
+    min-width: 120px;
+    font-size: 16px;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+    padding: 0 5px;
+    cursor: pointer;
+    min-width: 120px;
+`
+
+const Opcoes = styled.ul`
+    display: flex;
+`
 
 function OpcoesHeader() {
     return (
-        <ul className='opcoes'>
-            {textoOpcoes.map((texto) => (
-                <li
-                    className='opcao'
-                    key={texto}>
+        <Opcoes>
+            {textoOpcoes.map((texto, index) => (
+                <Opcao key={index}>
                     <p>{texto}</p>
-                </li>
+                </Opcao>
             ))}
-        </ul>
+        </Opcoes>
     )
 }
 
