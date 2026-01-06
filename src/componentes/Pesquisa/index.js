aa300c867d1f806f98c3b43adec62916e9b19800
Flávio Ghiraldeli Botelho De Paula <ghiraldeli26@gmail.com>
2026-01-06 12:20:56 -0300

    Using component Input instead of using <input/> HTML tag

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,9 +1,11 @@
+import Input from "../Input"
+
 function Pesquisa() {
     return (
         <section>
             <h2>Já sabe por onde começar?</h2>
             <h3>Encontre seu livro em nossa estante.</h3>
-            <input 
+            <Input 
                 placeholder="Escreva sua próxima leitura"
             />
         </section>
