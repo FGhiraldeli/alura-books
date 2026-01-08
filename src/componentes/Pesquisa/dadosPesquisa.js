24723a6d7143bbb9072210e086d05f6995223323
Flávio Ghiraldeli Botelho De Paula <ghiraldeli26@gmail.com>
2026-01-08 10:52:38 -0300

    Creating component Pesquisa that contains an array of books

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,9 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    { nome: 'Cinderela Chinesa', id: 1, src: livro },
+    { nome: 'O Exorcista', id: 2, src: livro },
+    { nome: 'Jurassic Park', id: 3, src: livro },
+    { nome: 'O Menino Maluquinho', id: 4, src: livro },
+    { nome: 'Um Girassol na Janela', id: 5, src: livro },
+]
