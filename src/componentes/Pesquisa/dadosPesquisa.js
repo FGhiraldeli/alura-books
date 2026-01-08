0bbf0840595900a426c1aaffdef277a5e2bdd35a
Flávio Ghiraldeli Botelho De Paula <ghiraldeli26@gmail.com>
2026-01-08 11:48:32 -0300

    Importing and adding the right images for the project

--- src/componentes/Pesquisa/dadosPesquisa.js
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -1,9 +1,13 @@
-import livro from '../../imagens/livro.png'
+import capa_cinderala_chinesa from '../../imagens/capa_cinderala_chinesa.jpg'
+import capa_jurassic_park from '../../imagens/capa_jurassic_park.jpg'
+import capa_o_exorcista from '../../imagens/capa_o_exorcista.jpg'
+import capa_o_menino_maluquinho from '../../imagens/capa_o_menino_maluquinho.jpg'
+import capa_um_girassol_na_janela from '../../imagens/capa_um_girassol_na_janela.jpg'
 
 export const livros = [
-    { nome: 'Cinderela Chinesa', id: 1, src: livro },
-    { nome: 'O Exorcista', id: 2, src: livro },
-    { nome: 'Jurassic Park', id: 3, src: livro },
-    { nome: 'O Menino Maluquinho', id: 4, src: livro },
-    { nome: 'Um Girassol na Janela', id: 5, src: livro },
+    { nome: 'Cinderela Chinesa', id: 1, src: capa_cinderala_chinesa },
+    { nome: 'O Exorcista', id: 2, src: capa_o_exorcista },
+    { nome: 'Jurassic Park', id: 3, src: capa_jurassic_park },
+    { nome: 'O Menino Maluquinho', id: 4, src: capa_o_menino_maluquinho },
+    { nome: 'Um Girassol na Janela', id: 5, src: capa_um_girassol_na_janela },
 ]
