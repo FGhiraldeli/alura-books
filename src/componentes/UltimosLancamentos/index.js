b605f6fd63b466c5533fa15c4afea930149b64f8
Flávio Ghiraldeli Botelho De Paula <ghiraldeli26@gmail.com>
2026-01-08 15:58:05 -0300

    Adding style to UltimosLancamentos component

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,13 +1,42 @@
 import { livros_lancamentos } from "./dadosUltimosLancamentos"
+import styled from "styled-components"
+
+const UltimosLancamentosContainer = styled.section`
+    background-color: #EBECEE;
+    padding-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+`
+
+const Titulo = styled.h2`
+    width: 100%;
+    padding: 30px 0;
+    background-color: #FFF;
+    color: #EB9B00;
+    font-size: 36px;
+    text-align: center;
+    margin: 0;
+`
+
+const NovosLivrosContainer = styled.div`
+    margin-top: 30px;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    cursor: pointer;
+`
+
 
 function UltimosLancamentos() {
     return (
-        <section>
-            <h2>ULTIMOS LANCAMENTOS</h2>
-            {livros_lancamentos.map((livro, index) => (
-                <img src={livro.src} key={index} alt=""/>
-            ))}
-        </section>
+        <UltimosLancamentosContainer>
+            <Titulo>ULTIMOS LANCAMENTOS</Titulo>
+            <NovosLivrosContainer>
+                {livros_lancamentos.map((livro, index) => (
+                    <img src={livro.src} key={index} alt="" />
+                ))}
+            </NovosLivrosContainer>
+        </UltimosLancamentosContainer>
     )
 }
 
