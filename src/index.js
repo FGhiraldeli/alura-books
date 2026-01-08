a2894f5a7f57d6b2eb7d69d0323335aa8bd76141
Flávio Ghiraldeli Botelho De Paula <ghiraldeli26@gmail.com>
2026-01-08 13:51:39 -0300

    Adding correct styles to const GlobalStyle

--- src/index.js
+++ src/index.js
@@ -6,13 +6,25 @@ import reportWebVitals from './reportWebVitals';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-  }
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 
-  li {
-    list-style: none;
-  }
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+
+li {
+  list-style: none;    
+}
+
+
 `
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
