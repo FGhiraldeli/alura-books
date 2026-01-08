576bb7670048bed52bba39b09fbe2ca0eae95e17
Flávio Ghiraldeli Botelho De Paula <ghiraldeli26@gmail.com>
2026-01-08 13:39:40 -0300

    Using styled-components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import UltimosLancamentos from './componentes/UltimosLancamentos';
 const AppContainer = styled.div`
       width: 100vw;
       height: 100vh;
-      background-image: linear-gradient(90deg, #002F52 35%, #326589 100%);
+      background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
     
     
 `
