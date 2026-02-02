ebdadbcb84b9a2cc3ffe0b32b8610467b88c7f15
Flávio Ghiraldeli Botelho De Paula <ghiraldeli26@gmail.com>
2026-02-02 11:43:26 -0300

    Creating a sample page

---
+++ src/rotas/Categorias.js
@@ -0,0 +1,23 @@
+
+import styled from 'styled-components';
+import Pesquisa from '../componentes/Pesquisa';
+
+
+const AppContainer = styled.div`
+      width: 100vw;
+      height: 100vh;
+      background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
+    
+    
+`
+
+
+function Categorias() {
+  return (
+    <AppContainer>
+      <Pesquisa />
+    </AppContainer>
+  );
+}
+
+export default Categorias;
